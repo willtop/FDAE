dd925207226d8152cf878cb6306b7fa98058308c
William Cui <billcuitop@gmail.com>
2024-09-05 16:12:59 -0400

    Create prepare_celeba.py

---
+++ prepare_celeba.py
@@ -0,0 +1,50 @@
+"""
+Using torchvision, download celebA dataset into a local folder
+Then arrange the folder by splits, into a pseudo class
+such that it's compatible with ImageFolder()
+"""
+
+import os
+from torchvision.datasets.celeba import CelebA
+
+CELEBA_DIR = "datasets/celeba"
+
+if __name__ == "__main__":
+    new_img_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), CELEBA_DIR)
+    os.makedirs(new_img_dir, exist_ok=True)
+    # load the CelebA using torchvision into the folder "celeba_dataset"
+    # no need to specify the label type or transformation here
+    _ = CelebA(CELEBA_DIR, 
+                split="all",
+                download=True)
+    
+    # arrange the images by split
+    orig_img_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                           CELEBA_DIR,
+                           "celeba",
+                           "img_align_celeba")
+
+    n_imgs = 202_599
+    n_train_imgs = 162_770
+    # move the images into the dataset folder
+    print("Moving training CelebA images...")
+    # the downloaded celeba images are named with idexing from 1
+    for i in range(1, n_train_imgs+1):
+        img_filename = f'{i:06d}.jpg'
+        old_img_loc = os.path.join(orig_img_dir, img_filename)
+        new_img_loc = os.path.join(new_img_dir, img_filename)
+        assert os.path.isfile(old_img_loc)
+        assert not os.path.isfile(new_img_loc)
+        os.rename(old_img_loc, new_img_loc)
+    
+    print("Moving testing CelebA images...")
+    for i in range(n_train_imgs+1, n_imgs+1):
+        img_filename = f'{i:06d}.jpg'
+        old_img_loc = os.path.join(orig_img_dir, img_filename)
+        new_img_loc = os.path.join(new_img_dir, img_filename)
+        assert os.path.isfile(old_img_loc)
+        assert not os.path.isfile(new_img_loc)
+        os.rename(old_img_loc, new_img_loc)
+
+
+    print("Script finished!")
