a6fa2acb824745da1bedabfc14e9aedfc2b51f31
William Cui <billcuitop@gmail.com>
2024-09-05 16:06:14 -0400

    Update train.sh

--- train.sh
+++ train.sh
@@ -2,15 +2,16 @@
 # set data_ind (0, 1, 2) to select dataset in data_dir_list
 # testing checkpoint
 # --resume_checkpoint logs/2023-12-15-21-52-50-mpi3d_real_complex_FDAE_seed0_rank0/model100000.pt --eval_only True
-data_ind=3
-data_dir_list=(datasets/shapes3d datasets/cars3d datasets/mpi3d_real_complex datasets/mpi3d_toy)
-# previously was (6,2,6)
-group_num_list=(6 2 10 7)
+data_ind=0
+data_dir_list=(datasets/shapes3d datasets/cars3d datasets/mpi3d_real_complex datasets/mpi3d_toy datasets/celeba)
+image_size_list=(64 64 64 64 224)
+group_num_list=(6 2 10 7 12)
 data_dir=${data_dir_list[${data_ind}]}
+image_size=${image_size_list[${data_ind}]}
 group_num=${group_num_list[${data_ind}]}
 
-content_decorrelation_weight=5e-5 #2.5e-5
-mask_entropy_weight=2.5e-4 #1.0e-4
+content_decorrelation_weight=2.5e-5
+mask_entropy_weight=1.0e-4
 
 max_step=50_000
 eval_interval=10_000
@@ -27,7 +28,7 @@ python fdae_train.py --log_suffix FDAE_seed${seed}_ \
 --additional_cond_map_layer_dim ${code_dim} \
 --max_step ${max_step} --save_interval ${save_interval} --eval_interval ${eval_interval} --attention_resolutions 32,16,8 \
 --global_batch_size ${batch} --lr 0.0001 \
---class_cond False --image_cond True --use_scale_shift_norm False --dropout 0.1 --image_size 64 \
+--class_cond False --image_cond True --use_scale_shift_norm False --dropout 0.1 --image_size ${image_size} \
 --num_channels 192 --num_head_channels 64 --num_res_blocks 3 --resblock_updown True --schedule_sampler lognormal \
 --use_fp16 True --weight_decay 0.0 --weight_schedule karras --debug_mode True \
 --seed ${seed} \
